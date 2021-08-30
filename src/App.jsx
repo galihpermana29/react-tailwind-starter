72bf9fb5db925f05efdf8660b6a01b6a4596f029
galihpermana29 <galihcbn123@gmail.com>
2021-08-30 11:31:12 +0700

    starter tailwind, react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className="">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-primary"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
